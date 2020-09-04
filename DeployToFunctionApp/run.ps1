b7d3242e76a98a807e6682ad692c53ff3dfe5837
Chris Gillum <cgillum@microsoft.com>
2020-09-03 18:25:06 -0700

    Fixed broken DeployToFunctionApp logic Upgraded project to v2 bundles Improved logging

--- DeployToFunctionApp/run.ps1
+++ DeployToFunctionApp/run.ps1
@@ -4,6 +4,40 @@ param($appInfo)
 
 #$appInfoHashTable = ConvertFrom-StringData -StringData $appInfo
 
+# ************Download from GitHub and create a zip file*********
+$targetDir = "$env:TEMP\dfperf-dedicated"
+
+# Delete the target directory if it already exists
+if (Test-Path -Path $targetDir) {
+    Write-Host "Deleting existing directory $targetDir..."
+    Remove-Item $targetDir -Recurse -Force
+}
+
+# Clone the project into the %TEMP% directory
+# Invoke git and capture both its stdout and stderr streams.
+Write-Host "Cloning https://github.com/cgillum/dfperf-scenarios into $targetDir..."
+$result = git clone https://github.com/cgillum/dfperf-scenarios $targetDir 2>&1
+if ($LASTEXITCODE) {
+    Throw "git failed (exit code: $LASTEXITCODE):`n$($result -join "`n")"
+}
+$result | ForEach-Object ToString
+
+# Build the project using the "publish" command so we can get the output for publishing
+Set-Location $env:TEMP\dfperf-dedicated
+Write-Host "Building project..."
+dotnet publish -p:DeployTarget=Package
+
+# Create the zip file for publishing
+$targetZipFilePath = "$env:TEMP\app.zip"
+if (Test-Path -Path $targetZipFilePath) {
+    Write-Host "Deleting existing $targetZipFilePath..."
+    Remove-Item $targetZipFilePath -ErrorAction Ignore -Force
+}
+$zipSrc = "$targetDir\bin\Debug\netcoreapp3.1\publish\"
+Write-Host "Zipping $zipSrc into $targetZipFilePath..."
+[System.IO.Compression.ZipFile]::CreateFromDirectory($zipSrc, $targetZipFilePath)
+Write-Host "$targetZipFilePath created successfully!"
+
 #"**********DEPLOY - Connecting to Azure Account***********"
 #$azureAplicationId ="a927d29b-a40a-4242-976e-e44af4c61ccc"
 #$azureTenantId= "72f988bf-86f1-41af-91ab-2d7cd011db47"
@@ -12,24 +46,7 @@ param($appInfo)
 #$DefaultProfile = Connect-AzAccount -Credential $psCred -TenantId $azureTenantId  -ServicePrincipal
 
 #"**********Deploying a zip file to Function***********"
-#Publish-AzWebApp -ResourceGroupName $appInfoHashTable.ResourceGroup -Name $appInfoHashTable.FunctionName -DefaultProfile $DefaultProfile -ArchivePath <zip file path> -Force
-
-# ************Download from GitHub and create a zip file*********
-
-# Clone the project into the %TEMP% directory
-Write-Host "Cloning https://github.com/cgillum/dfperf-scenarios..."
-git clone https://github.com/cgillum/dfperf-scenarios $env:TEMP\dfperf-dedicated
-
-# Build the project using the "publish" command so we can get the output for publishing
-#Set-Location $env:TEMP\dfperf-dedicated
-#dotnet publish -p:DeployTarget=Package
-
-# Create the zip file for publishing
-#Write-Host "Creating $env:TEMP\dfperf-dedicated\DFPerfScenarios\app.zip..."
-#[System.IO.Compression.ZipFile]::CreateFromDirectory("$env:TEMP\dfperf-dedicated\DFPerfScenarios\bin\Debug\netcoreapp3.1\publish\", "$env:TEMP\app.zip")
-
-# Get the path to the zip file (using dir to make sure it's actually there)
-#$zipFilePath = Get-ChildItem $env:TEMP -Filter *.zip | Select-Object -ExpandProperty FullName
+#Publish-AzWebApp -ResourceGroupName $appInfoHashTable.ResourceGroup -Name $appInfoHashTable.FunctionName -DefaultProfile $DefaultProfile -ArchivePath targetZipFilePath -Force
 
 # Triggering the many instances function
 #$payload = 100
